e5b809ab4a8e4d589d132465e391e4f2facda483
DanielBozanic <68564905+DanielBozanic@users.noreply.github.com>
2022-02-07 19:32:59 +0100

    Mala ispravka

--- sluzbenik/src/main/java/com/xml/sluzbenik/service/serviceImpl/IzvestajServiceImpl.java
+++ sluzbenik/src/main/java/com/xml/sluzbenik/service/serviceImpl/IzvestajServiceImpl.java
@@ -49,6 +49,7 @@ import com.xml.sluzbenik.security.TokenBasedAuthentication;
 import com.xml.sluzbenik.service.IzvestajService;
 import com.xml.sluzbenik.service.MarshallerService;
 import com.xml.sluzbenik.service.RDFService;
+import com.xml.sluzbenik.service.UnmarshallerService;
 import com.xml.sluzbenik.utils.ContextPutanjeKonstante;
 import com.xml.sluzbenik.utils.NamedGraphURIKonstante;
 import com.xml.sluzbenik.utils.XSDPutanjeKonstante;
@@ -58,6 +59,9 @@ public class IzvestajServiceImpl implements IzvestajService {
 	
 	@Autowired
 	private MarshallerService marshallerService;
+	
+	@Autowired
+	private UnmarshallerService unmarshallerService;
 	
 	@Autowired
 	private RDFService rdfService;
@@ -78,9 +82,8 @@ public class IzvestajServiceImpl implements IzvestajService {
 		}
 		String pronadjenIzvestajXml = izvestajRepository.pronadjiIzvestajXml(odDatumStr, doDatumStr);
 		if (pronadjenIzvestajXml != null) {
-			Izvestaj izvestaj = formirajIzvestaj(period);
-			izvestaj.setVocab("http://www.ftn.uns.ac.rs/rdf/izvestaj/");
-			izvestaj.setAbout("http://www.ftn.uns.ac.rs/rdf/izvestaj/" + odDatumStr + "/" + doDatumStr);
+			Izvestaj izvestaj = (Izvestaj) unmarshallerService.unmarshal(pronadjenIzvestajXml, ContextPutanjeKonstante.CONTEXT_PUTANJA_IZVESTAJ, 
+					XSDPutanjeKonstante.XSD_IZVESTAJ);
 			return izvestaj;
 		}
 		Izvestaj izvestaj = formirajIzvestaj(period);
