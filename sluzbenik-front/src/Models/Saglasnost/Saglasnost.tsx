ce34dff581f9dc25a9d5fd97de57a3dc7ad262b0
DanielBozanic <68564905+DanielBozanic@users.noreply.github.com>
2022-02-16 15:07:49 +0100

    RDF i JSON preuzimanje metapodataka front

--- sluzbenik-front/src/Models/Saglasnost/Saglasnost.tsx
+++ sluzbenik-front/src/Models/Saglasnost/Saglasnost.tsx
@@ -2,7 +2,8 @@ import { PacijentSaglasnost } from "./utils/PacijentSaglasnost";
 import { ZdravstveniRadnikSaglasnost } from "./utils/ZdravstveniRadnikSaglasnost";
 
 export interface Saglasnost {
-    "sa:PacijentSaglasnost": PacijentSaglasnost;
-    "sa:ZdravstveniRadnikSaglasnost": ZdravstveniRadnikSaglasnost;
-    "sa:DatumPodnosenja": string;
+	about: string;
+	"sa:PacijentSaglasnost": PacijentSaglasnost;
+	"sa:ZdravstveniRadnikSaglasnost": ZdravstveniRadnikSaglasnost;
+	"sa:DatumPodnosenja": string;
 }
